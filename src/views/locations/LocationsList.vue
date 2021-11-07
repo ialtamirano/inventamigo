bc8dc8f803f0c7e6c328fd66dfa2a8291050691b
Ivan Altamirano <ivan.a.altamirano@medtronic.com>
2021-11-07 00:51:36 -0700

    locations list

--- src/views/locations/LocationsList.vue
+++ src/views/locations/LocationsList.vue
@@ -12,31 +12,13 @@
     <div class="card events-card">
         <table class="table is-hoverable is-fullwidth ">
             <tbody>
-                    <tr>
+                <tr v-for="location in locations" :key="location.id">
                         <td width="5%"></td>
-                        <td><span class="is-size-5"> <router-link to="/locaciones/1">  Locaciones 1</router-link></span>  </td>
+                        <td><span class="is-size-5"> <router-link to="/locaciones/1">  {{location.name}}</router-link></span>  </td>
                         <td class="level-right"><button class="button  is-white" href="#"> <i class="fas fa-trash"></i></button></td>
-                    </tr>
-                    <tr>
-                        <td width="5%"><i class="fa fa-bell-o"></i></td>
-                        <td>Lorum ipsum dolem aire</td>
-                        <td class="level-right"><a class=" is-primary" href="#"><i class="fas fa-trash"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td width="5%"><i class="fa fa-bell-o"></i></td>
-                        <td>Lorum ipsum dolem aire</td>
-                        <td class="level-right"><a class=" is-primary" href="#"><i class="fas fa-trash"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td width="5%"><i class="fa fa-bell-o"></i></td>
-                        <td>Lorum ipsum dolem aire</td>
-                        <td class="level-right"><a class=" is-primary" href="#"><i class="fas fa-trash"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td width="5%"><i class="fa fa-bell-o"></i></td>
-                        <td>Lorum ipsum dolem aire</td>
-                        <td class="level-right"><a class=" is-primary" href="#"><i class="fas fa-trash"></i></a></td>
-                    </tr>
+                </tr>
+                    
+                    
             </tbody>
         </table>
                         
@@ -44,12 +26,22 @@
    
 </template>
 <script>
-
+import axios from "axios";
 
 
 export default {
   components: {
    
+  },
+  data: () =>({
+      locations : []
+  }),
+  mounted() {
+      
+        axios.get("http://localhost:8080/locations/").then((result) => {
+            this.locations = result.data.data;
+        });
+    
   }
 }
 </script>
